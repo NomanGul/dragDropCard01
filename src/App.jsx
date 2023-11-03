35c8c99e328a8ec83467d51b2a09f7a0049e78e4
nomangul <nomangul2001@gmail.com>
2023-11-03 17:19:39 +0500

    :sparkles: feat: add sortable + hover state + final touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,133 +1,108 @@
 import { useState } from "react";
+import { ReactSortable } from "react-sortablejs";
 import { Linkedin02Icon } from "./hugeicons/Stroke";
 import { NewTwitterIcon } from "./hugeicons/Stroke";
 import { GithubIcon } from "./hugeicons/Stroke";
-import Sortable from 'sortablejs';
 import masoomImg from "./assets/masoombahi.jpg";
 import nomanImg from "./assets/nomangul.jpg";
 import alexImg from "./assets/alex.jpg";
 import jamesImg from "./assets/james.jpg";
 
-
 import "./App.css";
 
 function App() {
-  
+  const [teamMembers, setTeamMembers] = useState([
+    {
+      id: 1,
+      avatar: nomanImg,
+      name: "Noman Gul",
+      title: "Full-Stack Dev",
+      socialLinks: [
+        {
+          icon: Linkedin02Icon,
+          url: "https://www.linkedin.com/in/noman-gul/",
+        },
+        {
+          icon: NewTwitterIcon,
+          url: "https://twitter.com/NomanGulKhan",
+        },
+        {
+          icon: GithubIcon,
+          url: "https://github.com/NomanGul",
+        },
+      ],
+    },
+    {
+      id: 2,
+      avatar: masoomImg,
+      name: "Masum Parvej",
+      title: "UX/UI Designer",
+      socialLinks: [
+        {
+          icon: NewTwitterIcon,
+          url: "https://twitter.com/masumparvej_",
+        },
+        {
+          icon: GithubIcon,
+          url: "https://github.com/halal-lab",
+        },
+      ],
+    },
+    {
+      id: 3,
+      avatar: alexImg,
+      name: "Alexander Smith",
+      title: "Product Manager",
+      socialLinks: [
+        {
+          icon: Linkedin02Icon,
+          url: "",
+        },
+        {
+          icon: NewTwitterIcon,
+          url: "",
+        },
+      ],
+    },
+    {
+      id: 4,
+      avatar: jamesImg,
+      name: "Melissa Taylor",
+      title: "Data Science",
+      socialLinks: [
+        {
+          icon: Linkedin02Icon,
+          url: "",
+        },
+        {
+          icon: GithubIcon,
+          url: "",
+        },
+      ],
+    },
+  ]);
+
   return (
-    <>
-      <div id="team-members">
-        <article className="team-member">
-          <img
-            className="team-member-avatar"
-            src={nomanImg}
-            alt="James Alexander"
-          />
+    <ReactSortable list={teamMembers} setList={setTeamMembers} id="team-members" animation={350} swap={false}>
+      {teamMembers.map((member) => (
+        <article key={member.id} className="team-member">
+          <img className="team-member-avatar" src={member.avatar} alt={member.name} />
           <div className="team-member-name">
-            <h3>Noman Gul</h3>
-            <p>Full-Stack Developer</p>
+            <h3>{member.name}</h3>
+            <p>{member.title}</p>
           </div>
           <ul className="social-links">
-            <li>
-              <a href="https://www.linkedin.com/in/noman-gul/" target="_blank">
-               
-                <Linkedin02Icon/>
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/NomanGulKhan" target="_blank">
-               
-                <NewTwitterIcon/>
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/NomanGul"  target="_blank">
-              
-                <GithubIcon />
-              </a>
-            </li>
+            {member.socialLinks.map((link, linkIndex) => (
+              <li key={linkIndex}>
+                <a href={link.url}>
+                  <link.icon />
+                </a>
+              </li>
+            ))}
           </ul>
         </article>
-        <article className="team-member">
-          <img
-            className="team-member-avatar"
-            src={masoomImg}
-            alt="Lilia Williams"
-          />
-          <div className="team-member-name">
-            <h3>Masum Parvej</h3>
-            <p>UX/UI Designer</p>
-          </div>
-          <ul className="social-links">
-            <li>
-              <a href="https://www.linkedin.com/in/masum-parvej/" target="_blank">
-              
-                <Linkedin02Icon/>
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/masumparvej_" target="_blank">
-               
-                <NewTwitterIcon/>
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/halal-lab" target="_blank">
-         
-                <GithubIcon />
-              </a>
-            </li>
-          </ul>
-        </article>
-        <article className="team-member">
-          <img className="team-member-avatar" src={alexImg} alt="Alexander Smith" />
-          <div className="team-member-name">
-            <h3>Alexander Smith</h3>
-            <p>Product Manager</p>
-          </div>
-          <ul className="social-links">
-            <li>
-              <a href="#">
-                {/* <img src="assets/linkedin.svg" alt="Linkedin" /> */}
-                <Linkedin02Icon/>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                {/* <img src="assets/x.svg" alt="X" /> */}
-                <NewTwitterIcon/>
-              </a>
-            </li>
-          </ul>
-        </article>
-        <article className="team-member">
-          <img className="team-member-avatar" src={jamesImg} alt="Melissa Taylor" />
-          <div className="team-member-name">
-            <h3>Melissa Taylor</h3>
-            <p>Data Science</p>
-          </div>
-          <ul className="social-links">
-            <li>
-              <a href="#">
-                {/* <img src="assets/linkedin.svg" alt="Linkedin" /> */}
-                <Linkedin02Icon/>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                {/* <img src="assets/x.svg" alt="X" /> */}
-                <NewTwitterIcon/>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                {/* <img src="assets/github.svg" alt="GitHub" /> */}
-                <GithubIcon />
-              </a>
-            </li>
-          </ul>
-        </article>
-      </div>
-    </>
+      ))}
+    </ReactSortable>
   );
 }
 
